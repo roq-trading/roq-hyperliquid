c301d96033ed9e3841fdfe7ffff84fc8994e0a2e
thraneh <thraneh@gmail.com>
2025-09-25 05:05:59 +0200

    update launch scripts

--- src/roq/hyperliquid/live.sh
+++ src/roq/hyperliquid/live.sh
@@ -12,21 +12,14 @@ CONFIG="${CONFIG:-$NAME}"
 
 CONFIG_FILE="$ROQ_CONFIG_PATH/roq-hyperliquid/$CONFIG.toml"
 
-URI="api.hyperliquid.xyz"
-
-REST_URI="https://$URI"
-WS_PUBLIC_URI="wss://$URI/ws"
-WS_PRIVATE_URI="wss://$URI/ws"
+FLAGFILE="../../../share/flags/prod/flags.cfg"
 
 $PREFIX ./roq-hyperliquid \
   --name "$NAME" \
   --config_file "$CONFIG_FILE" \
+  --flagfile "$FLAGFILE" \
   --cache_dir "$HOME/var/lib/roq/cache" \
   --event_log_dir "$HOME/var/lib/roq/data" \
-  --event_log_symlink true \
   --client_listen_address "$HOME/run/$NAME.sock" \
   --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
-  --rest_uri "$REST_URI" \
-  --ws_public_uri "$WS_PUBLIC_URI" \
-  --ws_private_uri "$WS_PRIVATE_URI" \
   $@
