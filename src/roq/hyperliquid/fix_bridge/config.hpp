0b0d1543278307c02e5fd17e9902469818360150
thraneh <thraneh@gmail.com>
2026-06-28 03:02:06 +0000

    moving fix_bridge into roq-server

---
+++ src/roq/hyperliquid/fix_bridge/config.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/hyperliquid/gateway/config.hpp"
+
+namespace roq {
+namespace hyperliquid {
+namespace fix_bridge {
+
+using Config = gateway::Config;
+
+}  // namespace fix_bridge
+}  // namespace hyperliquid
+}  // namespace roq
---
+++ src/roq/hyperliquid/fix_bridge/live.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "debug" ]; then
+  PREFIX="gdb --args"
+else
+  PREFIX=
+fi
+
+NAME="hyperliquid"
+
+CONFIG="${CONFIG:-$NAME}"
+
+CONFIG_FILE="$ROQ_CONFIG_PATH/roq-hyperliquid/$CONFIG.toml"
+
+FLAGFILE="../../../../share/flags/prod/flags.cfg"
+
+$PREFIX ./roq-hyperliquid-fix-bridge \
+  --name "$NAME" \
+  --exchange "hyperliquid" \
+  --dex "xyz" \
+  --config_file "$CONFIG_FILE" \
+  --flagfile "$FLAGFILE" \
+  --cache_dir "$HOME/var/lib/roq/cache" \
+  --event_log_dir "$HOME/var/lib/roq/data" \
+  --client_listen_address "$HOME/run/$NAME.sock" \
+  --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
+  $@
